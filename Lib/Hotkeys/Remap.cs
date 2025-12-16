85793cc4f42edaf8e7cbcaa3250dfb4c4c24a4f0
Stefano <schmidtsigma@gmail.com>
2025-12-16 05:43:18 +0300

    combine all 3 types of hotkeys into a unified function

--- Lib/Hotkeys/Remap.cs
+++ Lib/Hotkeys/Remap.cs
@@ -1,7 +1,3 @@
 ﻿namespace Lib.Hotkeys;
 
-public readonly record struct Remap(byte Mods, ushort Key)
-{
-	public static implicit operator Remap((byte Mods, ushort Key) remap) => new(remap.Mods, remap.Key);
-	public static implicit operator Remap(ushort key) => new(0, key);
-}
+public readonly record struct Remap(byte Mods, ushort Key);
